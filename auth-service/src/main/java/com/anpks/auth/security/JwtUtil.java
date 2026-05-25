9ae0b61c8479071022b085615d2471c38797b3ee
Anoop.KS <anoop@dryad.net>
2026-05-25 07:56:55 +0200

    lombok issue during maven compile fixed

--- auth-service/src/main/java/com/anpks/auth/security/JwtUtil.java
+++ auth-service/src/main/java/com/anpks/auth/security/JwtUtil.java
@@ -1,16 +1,16 @@
 package com.anpks.auth.security;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SecureDigestAlgorithm;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.util.Date;
-
-import javax.crypto.SecretKey;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
